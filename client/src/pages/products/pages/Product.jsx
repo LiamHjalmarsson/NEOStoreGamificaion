895238c33f8d3b3bebf429bdf7d4ac6821c14871
LiamHjalmarsson <Liam.hjalmarsson@hotmail.com>
2024-06-21 03:16:06 +0200

    products page

--- client/src/pages/products/pages/Product.jsx
+++ client/src/pages/products/pages/Product.jsx
@@ -11,9 +11,12 @@ const Product = () => {
     console.log(product);
     return (
         <div className='min-h-[85vh] flex-col w-full flex justify-center items-center max-lg:px-10'>
-            <div className='flex flex-col md:flex-row group transition duration-300 max-w-5xl bg-stone-300 shadow shadow-primary dark:bg-primary rounded-md w-full justify-center items-center h-[50vh] gap-12'>
+            <div className='flex flex-col md:flex-row group transition duration-300 max-w-5xl bg-stone-300 shadow shadow-primary dark:bg-primary rounded-md w-full justify-center items-center h-[70vh] md:h-[50vh] gap-6 md:gap-12'>
                 <div className='flex-grow max-w-[420px] md:h-full py-8 px-8 flex flex-col gap-4 justify-evenly'>
-                    <div className='flex flex-col gap-4'>
+                    <div className='flex flex-col gap-4 text-xl'>
+                        <h2 className='text-3xl font-semibold mb-4'>
+                            {product.title}
+                        </h2>
                         <p>
                             {
                                 "Brand: " + product.brand
@@ -29,6 +32,20 @@ const Product = () => {
                                 "Category: " + product.category
                             }
                         </p>
+                        <>
+                            <p>
+                                Sizes
+                            </p>
+                            <ul className='flex flex-wrap w-full justify-between gap-12 mt-1'>
+                                {
+                                    product.size.map((size, i) => (
+                                        <li className='p-2 border-stone-100 border-2 flex-grow text-center font-semibold'>
+                                            {size}
+                                        </li>
+                                    ))
+                                }
+                            </ul>
+                        </>
                     </div>
                 </div>
 
