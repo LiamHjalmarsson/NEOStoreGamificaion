e6465ef7542706395faa2a85896267b55b290d97
LiamHjalmarsson <Liam.hjalmarsson@hotmail.com>
2024-07-04 18:03:14 +0200

    fix bug of dark/light mode

--- client/src/components/layouts/navigation/components/icons/components/ThemeMode.jsx
+++ client/src/components/layouts/navigation/components/icons/components/ThemeMode.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { checkDarkTheme } from '../../../../../../utils/darkTheme';
 import { BsFillMoonFill, BsFillSunFill } from 'react-icons/bs';
 import Icon from '../Icon';
@@ -6,23 +6,24 @@ import Icon from '../Icon';
 const ThemeMode = () => {
     let [isDarkMode, setIsDarkMode] = useState(checkDarkTheme());
 
-    let darkModeHandler = () => {
-        setIsDarkMode(!isDarkMode);
-
+    useEffect(() => {
         if (isDarkMode) {
             document.documentElement.classList.add('dark');
+            localStorage.setItem('darkTheme', true);
         } else {
             document.documentElement.classList.remove('dark');
+            localStorage.setItem('darkTheme', false);
         }
+    }, [isDarkMode]);
 
-        localStorage.setItem('darkTheme', isDarkMode);
+    let darkModeHandler = (mode) => {
+        setIsDarkMode(mode);
     }
-
 
     return (
         <>
-            <Icon icon={<BsFillMoonFill />} onclick={darkModeHandler} custom={`${isDarkMode ? "delay-100" : "opacity-0 hidden"} `} />
-            <Icon icon={<BsFillSunFill />} onclick={darkModeHandler} custom={`${!isDarkMode ? "delay-100" : "opacity-0 hidden"}`} />
+            <Icon icon={<BsFillMoonFill />} onclick={() => darkModeHandler(true)} custom={`${!isDarkMode ? "delay-100" : "opacity-0 hidden"} `} />
+            <Icon icon={<BsFillSunFill />} onclick={() => darkModeHandler(false)} custom={`${isDarkMode ? "delay-100" : "opacity-0 hidden"}`} />
         </>
     );
 }
--- client/src/utils/darkTheme.js
+++ client/src/utils/darkTheme.js
@@ -1,8 +1,10 @@
 export const checkDarkTheme = () => {
-    let isDarkMode = localStorage.getItem("darkTheme") === "true";
+    let isDarkMode = JSON.parse(localStorage.getItem("darkTheme")) ? true : false;
 
     if (isDarkMode) {
         document.documentElement.classList.add('dark');
+    } else {
+        document.documentElement.classList.remove('dark');
     }
 
     return isDarkMode;
